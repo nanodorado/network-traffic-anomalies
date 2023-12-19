f374dd26f323a2ee423caf98444da1c36a1c83c1
nanodorado <nano_dorado@hotmail.com>
2023-12-18 22:49:05 -0300

    Added event correlation

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-# Network Anomaly Detection
+# Advanced Network Anomaly Detection
 
-This Python script utilizes the Isolation Forest algorithm to detect anomalies in network traffic data, which can be indicative of security threats.
+This Python script uses the Isolation Forest algorithm to detect anomalies in network traffic data, potentially indicative of security threats. It has been enhanced with temporal analysis, event correlation, and verification against a database of suspicious IP addresses.
 
-## Setup
+## Features
 
-To run this script, you need to have Python installed on your system along with the required packages.
+- **Anomaly Detection**: Uses Isolation Forest to identify anomalous patterns in network traffic.
+- **Temporal Analysis**: Analyzes traffic on an hourly basis to identify unusual patterns.
+- **Event Correlation**: Compares events across different data sets to identify suspicious behaviors.
+- **Suspicious IP List**: Compares detected IP addresses with a predefined list of IPs known for malicious activities.
+- **Notifications**: Implements a basic notification system to alert about detected anomalies.
 
-Install the required packages using:
+## Requirements
+
+To run this script, you will need Python and some libraries. Install the dependencies with the following command:
 
 ```bash
 pip install -r requirements.txt
--- anomaly_detection.py
+++ anomaly_detection.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import StandardScaler
+import datetime
+
+# Lista de IPs sospechosas para comparación (puede ser ampliada o integrada con una base de datos externa)
+SUSPICIOUS_IPS = ['192.168.1.100', '10.0.0.2']
 
 def load_data(file_path):
     data = pd.read_csv(file_path)
-    # Add any necessary preprocessing steps here
+    # Conversión de la marca de tiempo a un formato de fecha/hora si es necesario
+    data['timestamp'] = pd.to_datetime(data['timestamp'])
     return data
 
 def detect_anomalies(data, features):
@@ -17,12 +23,33 @@ def detect_anomalies(data, features):
     data['scores'] = model.decision_function(scaled_data)
     data['anomaly'] = model.predict(scaled_data)
     
-    return data[data['anomaly'] == -1]
+    return data
+
+def check_suspicious_ips(data, ip_column):
+    data['suspicious_ip'] = data[ip_column].isin(SUSPICIOUS_IPS)
+    return data[data['suspicious_ip']]
+
+def temporal_analysis(data):
+    data['hour'] = data['timestamp'].dt.hour
+    hourly_counts = data.groupby('hour').count()
+    return hourly_counts['timestamp']
+
+def send_notification(anomalies):
+    # Esta función podría ser más compleja dependiendo del método de notificación
+    print("Alerta de seguridad: Se han detectado anomalías")
 
 if __name__ == "__main__":
     file_path = 'path_to_network_traffic_data.csv'
     network_data = load_data(file_path)
     anomalies = detect_anomalies(network_data, ['src_bytes', 'dst_bytes'])
     
-    print("Detected Anomalies:")
-    print(anomalies)
+    if not anomalies.empty:
+        send_notification(anomalies)
+    
+    suspicious_activities = check_suspicious_ips(anomalies, 'src_ip')
+    print("Actividades Sospechosas:")
+    print(suspicious_activities)
+
+    hourly_distribution = temporal_analysis(network_data)
+    print("Distribución de tráfico por hora:")
+    print(hourly_distribution)
